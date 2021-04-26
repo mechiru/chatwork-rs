bc9922c7723cfd83a584e751f013c0a863e49052
mechiru <46036584+mechiru@users.noreply.github.com>
2021-04-27 00:48:54 +0900

    rename to url encode error

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("query params serialization error: {0}")]
-    QueryParams(#[from] serde_urlencoded::ser::Error),
+    #[error("url encoding error: {0}")]
+    UrlEncoding(#[from] serde_urlencoded::ser::Error),
     #[error("http client error: {0}")]
     Http(#[from] reqwest::Error),
     #[error("http status code error: {0}")]
